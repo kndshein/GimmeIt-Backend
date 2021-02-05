cb4e12019b8f3f24056598c01de230cd76b5b8f6
Clayton Miller <clayton.miller@loop.colum.edu>
2021-02-05 16:42:11 -0600

    itemModel - adds schema

---
+++ models/itemModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("../db/connection");
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    img: String, //Change to array later
+    name: { type: String, required: true },
+    description: String,
+    available: { 
+        type: Boolean, required: true , 
+        default: true 
+    },
+    shipping: String,
+    donor: {
+        type: Schema.Types.ObjectId,
+        ref: "Donor",
+    }
+})
+
+const Item = mongoose.model("Item", itemSchema)
+module.exports = Item
